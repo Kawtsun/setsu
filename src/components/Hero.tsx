94d556c49869fa4731ffb8f6886a41509a1ceacc
Kawtsun <morpheusjoshua71@gmail.com>
2024-12-22 20:03:21 +0800

    Home Ok

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
- import logo from "../assets/setsu.png";
- import bg from "../assets/setsubg.png";
+import { Link } from "react-router";
+import logo from "../assets/setsu.png";
+import bg from "../assets/setsubg.png";
 
 
 function Hero() {
@@ -7,10 +8,16 @@ function Hero() {
         <>
             <div className="relative p-12">
                 <img className="absolute inset-0 w-full h-full object-cover opacity-80" src={bg} alt="bg" />
-                <div className="relative mx-28 space-y-2 bg-opacity-80 bg-slate-200 p-10 rounded-lg">
-                    <h1 className="font-bold text-5xl">Setsuna Yuki</h1>
-                    <h2 className="text-lg">Member of Nijigasaki High School Idol Club (Former Student Council President)</h2>
-                    <img className="w-64 rounded-full" src={logo} alt="logo"/>
+                <div className="relative mx-28 bg-opacity-80 bg-slate-200 p-10 rounded-lg">
+                    <h1 className="font-bold text-5xl mb-3">Setsuna Yuki</h1>
+                    <h2 className="text-lg">Member of Nijigasaki High School Idol Club <span className="italic">(Former Student Council President)</span></h2>
+                    <img className="w-64 rounded-full mt-5" src={logo} alt="logo"/>
+                </div>
+            </div>
+            <div className="mx-auto w-max  my-10 p-10 text-center space-y-5 bg-slate-50 rounded-lg text-lg">
+                <h3 className="">Interested to learn more about this idol?</h3>
+                <div className="py-2 px-5 border w-max mx-auto rounded-md text-white font-medium bg-blue-700 hover:bg-blue-600 ease-in-out duration-300">
+                    <Link to="/about">Explore</Link>
                 </div>
             </div>
         </>
