4433bf0e2408401a39cfb1043d93c81306278f74
Kawtsun <morpheusjoshua71@gmail.com>
2024-12-22 22:06:40 +0800

    improve nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,21 +6,20 @@ function Navbar() {
 
     return (
         <nav className="flex justify-between items-center py-3 px-20 bg-red-700 text-slate-200">
-            <h1 className="font-Inter font-semibold text-4xl tracking-wide py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300">
+            <h1 className="font-Inter font-semibold text-4xl tracking-wide py-1 px-3 rounded-md">
                 <Link to="/">Setsu</Link>
             </h1>
-            <ul className="flex list-none">
-                <li className={`mr-6 text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300 ${location.pathname === '/' ? 'text-yellow-400' : ''}`}>
+            <ul className="flex list-none gap-4">
+                <li className={`text-lg font-medium py-1 px-4 rounded-md hover:text-slate-300 ease-in-out duration-300 ${location.pathname === '/' ? 'text-yellow-400' : ''}`}>
                     <Link to="/">Home</Link>
                 </li>
-                <li className={`mr-6 text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300 ${location.pathname === '/about' ? 'text-yellow-400' : ''}`}>
+                <li className={`text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300 ${location.pathname === '/about' ? 'text-yellow-400' : ''}`}>
                     <Link to="/about">About</Link>
                 </li>
-                <li className={`mr-6 text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300 ${location.pathname === '/seiyuu' ? 'text-yellow-400' : ''}`}>
+                <li className={`text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300 ${location.pathname === '/seiyuu' ? 'text-yellow-400' : ''}`}>
                     <Link to="/seiyuu">Seiyuu</Link>
                 </li>
             </ul>
-            
         </nav>
     )
 }
