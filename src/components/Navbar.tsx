b916f9fae96d088e7d504c4263fa7d1938c48c66
Kawtsun <morpheusjoshua71@gmail.com>
2024-12-22 20:52:27 +0800

    add navbar active

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,22 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 function Navbar() {
+
+    const location = useLocation();
+
     return (
-        <nav className="flex justify-between items-center py-3 px-20 bg-red-700 text-white">
+        <nav className="flex justify-between items-center py-3 px-20 bg-red-700 text-slate-200">
             <h1 className="font-Inter font-semibold text-4xl tracking-wide py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300">
                 <Link to="/">Setsu</Link>
             </h1>
             <ul className="flex list-none">
-                <li className="mr-6 text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300">
+                <li className={`mr-6 text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300 ${location.pathname === '/' ? 'text-yellow-400' : ''}`}>
                     <Link to="/">Home</Link>
                 </li>
-                <li className="mr-6 text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300">
+                <li className={`mr-6 text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300 ${location.pathname === '/about' ? 'text-yellow-400' : ''}`}>
                     <Link to="/about">About</Link>
                 </li>
-                <li className="mr-6 text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300">
+                <li className={`mr-6 text-lg font-medium py-1 px-3 rounded-md hover:text-slate-300 ease-in-out duration-300 ${location.pathname === '/seiyuu' ? 'text-yellow-400' : ''}`}>
                     <Link to="/seiyuu">Seiyuu</Link>
                 </li>
             </ul>
