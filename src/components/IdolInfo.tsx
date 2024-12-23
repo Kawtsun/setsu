e4d1fedf35786356004ce2251a5200956c1817b9
Kawtsun <morpheusjoshua71@gmail.com>
2024-12-23 23:57:04 +0800

    Idol Info Done

--- src/components/IdolInfo.tsx
+++ src/components/IdolInfo.tsx
@@ -2,7 +2,7 @@ import setsubg from "../assets/setsuinfobg.png";
 
 export default function IdolInfo() {
   return (
-    <div className="w-max mx-auto" style={{ maxWidth: "1000px" }}>
+    <div className="w-max mx-auto bg-white py-3 px-10 text-opacity-80 text-black" style={{ maxWidth: "1000px" }}>
       {/*intro img*/}
       <div className="my-6">
         <img
@@ -77,7 +77,7 @@ export default function IdolInfo() {
           </div>
         </div>
         {/*Table Heading and Data */}
-        <div className="flex flex-row justify-between border-t-2 border-gray-500 w-full py-4 px-4 text-lg">
+        <div className="flex flex-row justify-between border-t-2 border-b-2 border-gray-500 w-full py-4 px-4 text-lg">
           <div>
             <strong>Height</strong>
           </div>
@@ -85,9 +85,55 @@ export default function IdolInfo() {
             <span>1m 54cm | <small>5'</small></span>
           </div>
         </div>
-        
-    
-        
+        {/*Other information*/}
+        <div className="my-5 space-y-5 text-lg">
+          <h2 className="text-2xl font-bold">Background</h2>
+          <p>
+            When Setsuna saw school idols for the first time, it felt like "fate" was calling out to her; the very reason for becoming a school idol herself. Seemingly always busy with school idol activities, Setsuna had become somewhat of an urban legend among the students at Nijigasaki, with many claiming they had never even seen her at school. Setsuna would always wear her idol outfit whenever she is with the Nijigasaki High School Idol Club.
+          </p>
+          <p>
+            But the girl that everyone knew as "Setsuna Yuki" had actually adopted that name herself, so that her parents would not figure out she was an idol. Setsuna comes from a strict household, where things like anime, manga, and school idols are banned. One reason why she became an urban legend is linked to the fact that she attends the same school as Nana Nakagawa, who has a different appearance than Setsuna.
+          </p>
+          <p>
+            Along with Karin Asaka and Kasumi Nakasu, she works with Dengeki Online. The name "Setsuna Yuki" came from the characters of a light novel that Nana read.
+          </p>
+
+          <h2 className="text-2xl font-bold">Background</h2>
+          <p>
+            Setsuna has an energetic smile and a distinctive style of performing. She's very friendly, charismatic and naturally people oriented. She is easily excitable and full of energy, and she can be moved emotionally very easily. She lights up whenever she is asked a question about her interests, which include anime, manga, video games, and school idols. While she is a very passionate geek and loves to talk about her otaku hobbies with her friends, due to her status as a serious and hard-working student council president, she feels she has to hide them from her mom. When the latter learns about Setsuna, she is happy with it and cheerfully goes to the concert.
+          </p>
+          <p>
+            She takes her passions very seriously, and with regards to being a school idol, always wants to improve. She wants to share her love of school idols with the world, and help others feel more comfortable saying what they love, but she can be too passionate about idol activities, the reason that drove the members of the original School Idol Club apart and forcing her to disband the club afterwards.
+          </p>
+          <p>
+            As Nana, she also strives to do well in school, so she always does homework as soon as possible and studies on her off days.
+          </p>
+          <p>
+            Setsuna is also very kind and helpful, and is shown to be very supportive of her friends. For example, in the Dengeki 4-koma, she continued to eat Kasumi's spicy bread in order to not let her down since she cooked it for her and Karin. She tries to encourage everyone to pursue their dreams and is open to helping others with their dancing and singing.
+          </p>
+
+          <h2 className="text-2xl font-bold">Clubs and Hobbies</h2>
+          <p>
+            Despite not being good at it, Setsuna likes cooking, but when she asks how it tastes, she never gets an answer. Some of her friends say that her cooking's taste is "a bit too original".
+          </p>
+          <p>
+            She likes everything involving school idols and is very knowledgeable about them. She likes every subject in school except health class.
+          </p>
+          <p>
+            Setsuna is very interested in anime, manga, and video games. She can get very excited and talk at length about anime concepts and her favorite series. During weekends, Setsuna might consume about five hours of anime throughout the day and spend about three hours playing video games.
+          </p>
+          <p>
+            Setsuna is the student council president of Nijigasaki High School as Nana Nakagawa, although she loses her position to Shioriko Mifune in chapter nine of the School Idol Festival ALL STARS story.
+          </p>
+
+          <h2 className="text-2xl font-bold">Appearance</h2>
+          <p>
+            Setsuna is rather short with a slim figure. She has long bluish dark gray hair that she partly ties into a ponytail on the right side with a rose-shaped ornament and light grey eyes.
+          </p>
+          <p>
+            As <b>Nana Nakagawa</b>, she wears glasses and ties her hair into two low pigtail braids instead.
+          </p>
+        </div>
       </div>
     </div>
   );
